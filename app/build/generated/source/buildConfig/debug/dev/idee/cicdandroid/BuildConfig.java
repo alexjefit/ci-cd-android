cfcbb1ac392f3dbd6ea69b2571dc9c04cd3896c3
alexjefit <alex@jefit.com>
2022-12-15 20:04:45 -0800

    init commit

---
+++ app/build/generated/source/buildConfig/debug/dev/idee/cicdandroid/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dev.idee.cicdandroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "dev.idee.cicdandroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 210010003;
+  public static final String VERSION_NAME = "1.0.3";
+  // Fields from default config.
+  public static final String API_KEY = "420759c6d3a9ed43a042e512f8c8020d";
+}
